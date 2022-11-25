a8e962b4d68d0968b319cd9b4c60a6e5b77df82a
BiscuitCleaner <bobpopreal@outlook.com>
2022-11-26 00:27:11 +0800

    更新

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const mineflayer = require('mineflayer')
+
+var bot = mineflayer.createBot(
+    {
+        host: 'localhost',
+        username: 'epicgamerberry@outlook.com',
+        password: '',
+        version: '1.19.2',
+        auth: 'microsoft'
+    }
+)
+
+let ready = false
+
+bot.once('spawn', () => {
+    ready = true
+    console.log("Bot is ready!")
+})
+
+bot.on('kicked', (reason, loggedIn) => {
+    console.log(`Bot got kicked\n Reason: ${reason}`)
+})
+
+bot.on('error', (err)=> {
+    console.log(`Bot got kicked\n Reason: ${err}`)
+})
