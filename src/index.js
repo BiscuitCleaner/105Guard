3684a6bc7d68b5280e212764cae5b4c5eb1b8c66
BiscuitCleaner <bobpopreal@outlook.com>
2022-11-27 22:30:22 +0800

    更新

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ const mineflayer = require('mineflayer')
 let config = require("../config.json")
 var bot = mineflayer.createBot(
     {
-        host: 'localhost',
+        host: config.host,
         username: config.username,
-        
         password: '',
         version: '1.19.2',
         auth: 'microsoft'
@@ -19,10 +18,18 @@ bot.once('spawn', () => {
     console.log("Bot is ready!")
 })
 
+setInterval(() => {
+    if (ready == true) {
+        bot.chat(`/visit #${config.island}`)
+    }
+}, 50000);
+
 bot.on('kicked', (reason, loggedIn) => {
     console.log(`Bot got kicked\n Reason: ${reason}`)
+    process.exit(3)
 })
 
 bot.on('error', (err)=> {
     console.log(`Bot got kicked\n Reason: ${err}`)
+    process.exit(3)
 })
